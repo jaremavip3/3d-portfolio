f1764e2695083d2b310366e6901b9736098c20e5
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-03-11 15:29:52 -0400

    Update dodecahedron model and enhance Model component to include materials and preload GLB file

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,11 +1,14 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function Model() {
-  const { nodes } = useGLTF("/media/dodecahedron.glb");
+  const { nodes, materials } = useGLTF("/media/dodecahedron.glb");
 
+  console.log("dodecahedron MESH: ");
+  console.log(nodes);
   return (
-    <group>
-      <mesh></mesh>
+    <group dispose={null}>
+      <mesh {...nodes.Object_2} />
     </group>
   );
 }
+useGLTF.preload("/media/dodecahedron.glb");
