f3775dbe70550dc0feea958cc1ea2781aa16d501
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-03-11 14:21:02 -0400

    Add dodecahedron model and update dependencies for GLTF support - downloaded react-three/drei

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,4 +1,8 @@
+import { useGLTF } from "@react-three/drei";
+
 export default function Model() {
+  const { nodes } = useGLTF("/media/dodecahedron.glb");
+
   return (
     <group>
       <mesh></mesh>
