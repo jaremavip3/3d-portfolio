9ddc228a4a7c94a3bf1ed91480ae0f300e11e95f
Yarema Petrushchak <yarema.petrushchak24@gmail.com>
2025-03-13 14:22:36 -0400

    Refactor Scene component: remove Model import and implement Dodecahedron mesh with rotation animation

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,12 +1,28 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
-import Model from "./Model";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 export default function Scene() {
+  const dodecahedronRef = useRef();
+
+  const Dodecahedron = ({ ...props }) => {
+    useFrame((state, delta) => {
+      dodecahedronRef.current.rotation.x += delta;
+      console.log("dodecahedronRef.current.rotation.x: ", dodecahedronRef.current.rotation.x);
+    });
+    return (
+      <mesh {...props}>
+        <dodecahedronGeometry />
+        <meshStandardMaterial color="hotpink" />
+        <ambientLight />
+      </mesh>
+    );
+  };
+
   return (
     // everything that it inside Canvas is inside WebGL, so it will be rendered by the GPU
     <Canvas>
-      <Model />
+      <Dodecahedron ref={dodecahedronRef} position={[0, 0, 0]} size={[1, 1, 1]} />
     </Canvas>
   );
 }
